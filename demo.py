e3fb3a571cc8ac9066f23c19772e790627075c30
rore <rorezf@163.com>
2018-07-08 15:57:53 +0800

    allure test

--- demo.py
+++ demo.py
@@ -10,13 +10,15 @@ allure.environment(browser="chrome")
 class Test_demo:
 	@allure.story("step_one")
 	def test_one(self):
-		print "test go"
-		with allure.step("first"):
-			allure.attach("login")
-			allure.attach("click")
-		with allure.step("secondly"):
-			allure.attach("expect:1")
-			assert 1 == 1
+		# with allure.step("first"):
+		# 	allure.attach("login")
+		# 	allure.attach("click")
+		# with allure.step("secondly"):
+		# 	allure.attach("expect:1")
+		allure.step('first')
+		assert 1 == 1
+		allure.step("secondly")
+		assert 2 == 2
 
 	def test_two(self):
 		assert 1 == 1	
