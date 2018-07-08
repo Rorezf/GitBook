6d7a792dc32958c8471590cad1fb4bb1a04a3fc5
rore <rorezf@163.com>
2018-07-08 15:50:53 +0800

    allure test

--- demo.py
+++ demo.py
@@ -3,9 +3,20 @@
 import pytest
 import allure
 
+allure.environment(host="127.0.0.1")
+allure.environment(browser="chrome")
+
+@allure.feature("demo test")
 class Test_demo:
+	@allure.story("step_one")
 	def test_one(self):
-		assert 1 == 2
+		print "test go"
+		with allure.step("first"):
+			allure.attach("login")
+			allure.attach("click")
+		with allure.step("secondly"):
+			allure.attach("expect:1")
+			assert 1 == 1
 
 	def test_two(self):
 		assert 1 == 1	
