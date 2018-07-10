d255389725c753291116295d90435e62e33875aa
luozhifeng <luozhifeng@casachina.com.cn>
2018-07-10 17:21:02 +0800

    allure test

--- demo.py
+++ demo.py
@@ -27,7 +27,7 @@ class Test_demo:
 		assert 1 == 1	
 
 @allure.feature("demo test 2")
-@allure.testcase("https://www.baidu.com")
+@allure.testcase("https://192.168.55.192")
 class Test_demo_2:
 	@allure.story("step_one")
 	def test_one(self):
