e511b5dd8d1d829c21296626472065b7088c32a7
luozhifeng <luozhifeng@casachina.com.cn>
2018-07-10 10:13:39 +0800

    allure test

--- demo.py
+++ demo.py
@@ -12,7 +12,7 @@ class Test_demo:
 	@allure.story("step_one")
 	def test_one(self):
 		allure.step('first')
-		assert 1 == 1
+		assert 1 == 2
 		allure.step("secondly")
 		assert 2 == 2
 
@@ -33,7 +33,7 @@ class Test_demo_2:
 		allure.step('first')
 		assert 1 == 1
 		allure.step("secondly")
-		assert 2 == 2
+		assert 2 == 3
 
 	@allure.story("step_two")
 	def test_two(self):
